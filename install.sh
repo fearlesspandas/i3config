5b9b9adac7f9660f39c5e93b2cd8dfbe423e10a3
landon <landonrenzullo@gmail.com>
2025-06-29 22:09:53 -0400

    nvim config tweaks

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ case $1 in
 		case $2 in
 			"i3")
 				echo "applying i3 config + i3 status bar config" &\
+					cp -R ./i3status ~/.config &\
 					cp -R ./i3 ~/.config;;
 			"alacritty")
 				echo "applying alactritty config"&\
--- nvim/init.lua
+++ nvim/init.lua
@@ -14,6 +14,10 @@ vim.lsp.enable('rust_analyzer')
 vim.cmd[[
 	highlight Normal guibg=none
 	highlight NonText guibg=none
-	highlight Normal ctermbg=none
+	highlight Normal ctermbg=DarkGray
 	highlight NonText ctermbg=none
+	highlight MatchParen ctermbg=blue guibg=lightblue
+	highlight Directory ctermbg=darkyellow guibg=darkyellow
+	highlight CursorLine cterm=NONE ctermbg=DarkGray guibg=#3c3c3c
+	highlight Cursor cterm=NONE ctermbg=DarkGray guibg=#3c3c3c
 ]]
