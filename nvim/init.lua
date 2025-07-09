22bee43333a10cc07477bb344b75846bf5e8ee85
Landon <landonrenzullo@gmail.com>
2025-07-09 17:04:07 -0400

    small fixes

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,6 @@ vim.cmd[[
 	highlight Normal ctermbg=DarkGray
 	highlight NonText ctermbg=none
 	highlight MatchParen ctermbg=blue guibg=lightblue
-	highlight Directory ctermbg=darkyellow guibg=darkyellow
 	highlight CursorLine cterm=NONE ctermbg=DarkGray guibg=#3c3c3c
 	highlight Cursor cterm=NONE ctermbg=DarkGray guibg=#3c3c3c
 ]]
